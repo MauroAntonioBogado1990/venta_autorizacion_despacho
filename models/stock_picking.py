b85561fc58dd2df7f9ce8395f19ce621a4a77469
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2025-11-07 15:06:05 -0300

    change string in picking

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    autorizado = fields.Boolean(string='Autorizado por ventas')
+    autorizado = fields.Boolean(string='Autorizado')
