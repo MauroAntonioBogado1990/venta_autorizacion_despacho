ecd3ed41af04badb9d611f73c486d954c8f7224c
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2025-11-07 14:53:12 -0300

    add of write in the module about sale_order

--- models/sale_order.py
+++ models/sale_order.py
@@ -6,10 +6,20 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     autorizado = fields.Boolean(string='Autorizado', groups='venta_autorizacion_despacho.group_autorizador_ventas')
-
-    def _action_confirm(self):
-        res = super()._action_confirm()
-        for order in self:
-            for picking in order.picking_ids:
-                picking.autorizado = order.autorizado
+    
+    #Esto en caso de confirmacion
+    # def _action_confirm(self):
+    #     res = super()._action_confirm()
+    #     for order in self:
+    #         for picking in order.picking_ids:
+    #             picking.autorizado = order.autorizado
+    #     return res
+    
+    #Esto en caso cuando se escribe
+    def write(self, vals):
+        res = super().write(vals)
+        if 'autorizado' in vals:
+            for order in self:
+                for picking in order.picking_ids:
+                    picking.autorizado = vals['autorizado']
         return res
