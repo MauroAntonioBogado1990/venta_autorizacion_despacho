8a0a2069fd227a072855e885c9722e089e8c6752
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2025-11-07 15:40:13 -0300

    add new visialitation

--- models/sale_order.py
+++ models/sale_order.py
@@ -5,8 +5,8 @@ from odoo import models, fields, api, _
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    autorizado = fields.Boolean(string='Autorizado', groups='venta_autorizacion_despacho.group_autorizador_ventas')
-    
+    #autorizado = fields.Boolean(string='Autorizado', groups='venta_autorizacion_despacho.group_autorizador_ventas')
+    autorizado = fields.Boolean(string='Autorizado')
     #Esto en caso de confirmacion
     # def _action_confirm(self):
     #     res = super()._action_confirm()
